aaac70d1342caf0a8400b19f91f8689aae9f963f
Muninn <2545908841@qq.com>
2025-05-08 22:43:30 +0800

    test：完成定位接口的测试，相关注意事项已写入README

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ WhiteDeer白鹿打卡系统-后端
 
 Finished：
 * 依赖项配置及初始化
-* Java调用python人脸识别算法（已测试 存在魔法路径）
+* 调用人脸识别算法（已测试 存在魔法路径）
+* 调用腾讯地图api定位（已测试 需要进一步优化到卫星ip双定位提高精度与准确性）
 
 TODO:
 * 用户相关服务
@@ -11,5 +12,5 @@ TODO:
 * 验证码及二维码服务
 
 Caution:
-* 服务器本身需要配置好Python环境（opencv）才能正确进行人脸识别
-  * 例如pip安装opencv与pil
+* 服务器本身需要配置好Python环境（opencv）才能正确进行人脸识别与ip定位
+  * 例如pip安装opencv，pil，request模块
--- src/main/java/com/WhiteDeer/Test.java
+++ src/main/java/com/WhiteDeer/Test.java
@@ -4,8 +4,17 @@ import com.WhiteDeer.service.PyAPI;
 
 public class Test {
     public static void main(String[] args) {
-        PyAPI.trainFaceLabels(String.valueOf(2),"C:\\Code\\test2.jpg");//魔法路径
-        boolean a = PyAPI.faceRecognition(String.valueOf(2),"C:\\Code\\test1.jpg");//魔法路径
-        System.out.println(a);
+
     }
 }
+//人脸识别测试
+//    public static void main(String[] args) {
+//        PyAPI.trainFaceLabels(String.valueOf({id}), {path});//魔法路径
+//        boolean a = PyAPI.faceRecognition(String.valueOf({id}),{path});//魔法路径
+//        System.out.println(a);
+//    }
+
+//定位测试
+//public static void main(String[] args) {
+//    System.out.println(PyAPI.geoFencing());
+//}
