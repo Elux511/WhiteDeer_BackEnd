ba36ca78379d521b82fa8debe92e29f1b9067a9a
jzs_cocoon <1987794678@qq.com>
2025-05-09 18:51:44 +0800

    feat:增加了创建团队的用户功能

--- src/main/java/com/WhiteDeer/service/UserService.java
+++ src/main/java/com/WhiteDeer/service/UserService.java
@@ -1,8 +1,13 @@
 package com.WhiteDeer.service;
 
 import com.WhiteDeer.Group;
+import com.WhiteDeer.GroupMember;
 import com.WhiteDeer.Task;
 import com.WhiteDeer.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.time.LocalTime;
 
 public class UserService {
     User user;
@@ -38,5 +43,25 @@ public class UserService {
     }
     public void deleteNo(Task task){
         user.deleteNo(task.getId());
+    }
+
+    //创建一个团队
+    public void createGroup(String group_name, User user, String introduction){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Group group = (Group)ctx.getBean("Group");
+        //group.setId();
+        group.setName(group_name);
+        group.setIntroduction(introduction);
+
+        //把自己加进团队成员中
+        GroupMember member = (GroupMember) ctx.getBean("groupMember");
+        member.setUser(user);
+        member.setGroup(group);
+        member.setMember_type(GroupMember.Type.member);
+        member.setJoin_time(LocalTime.now());
+        group.addMember(member);
+
+        //把团队加进自己所在团队列表
+        user.addGroup(group.getId());
     }
 }
