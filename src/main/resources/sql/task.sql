39f0b85c8c8f1a7e9006bbea4fbe68cb936bbcc6
hyh <1786171163@qq.com>
2025-05-10 12:19:53 +0800

    feat: datagrip的sql文件

--- src/main/resources/sql/task.sql
+++ src/main/resources/sql/task.sql
@@ -1,6 +1,7 @@
 create table task
 (
-    id                  varchar(8)   not null        primary key,
+    id                 varchar(8)   not null
+        primary key,
     name               varchar(255) null,
     groupId            varchar(255) null,
     beginTime          time         null,
@@ -9,14 +10,4 @@ create table task
     completedUserIds   text         null,
     uncompletedUserIds text         null
 );
-DELIMITER $$
-CREATE TRIGGER before_insert_task_table
-    BEFORE INSERT ON task_table
-    FOR EACH ROW
-BEGIN
-    IF NEW.id IS NULL THEN
-        SET NEW.id = UUID();
-END IF;
-END$$
-DELIMITER ;
 
--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -1,6 +1,7 @@
 create table user
 (
-    id  varchar(8)   not null    primary key,
+    id          varchar(8)   not null
+        primary key,
     name        varchar(255) null,
     phoneNumber varchar(20)  null,
     password    varchar(255) null,
@@ -8,15 +9,4 @@ create table user
     yesTaskSet  text         null,
     noTaskSet   text         null
 );
-
-DELIMITER $$
-CREATE TRIGGER before_insert_user_table
-    BEFORE INSERT ON user_table
-    FOR EACH ROW
-BEGIN
-    IF NEW.id IS NULL THEN
-        SET NEW.id = UUID();
-END IF;
-END$$
-DELIMITER ;
 
