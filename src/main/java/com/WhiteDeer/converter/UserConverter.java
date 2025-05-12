1a22a9cbf34971a5e8d59b5ce32d72ddaa5b8bc3
jzs_cocoon <1987794678@qq.com>
2025-05-12 21:30:25 +0800

    feat:user1.3

--- src/main/java/com/WhiteDeer/converter/UserConverter.java
+++ src/main/java/com/WhiteDeer/converter/UserConverter.java
@@ -12,6 +12,10 @@ public class UserConverter {
         userDTO.setPassword(user.getPassword());
         userDTO.setPhoneNumber(user.getPhoneNumber());
         userDTO.setFace(user.getFace());
+        userDTO.setJoinGroupSet(user.getJoinGroupSet());
+        userDTO.setCreateGroupSet(user.getCreateGroupSet());
+        userDTO.setYesTaskSet(user.getYesTaskSet());
+        userDTO.setNoTaskSet(user.getNoTaskSet());
         return userDTO;
     }
 
@@ -21,6 +25,10 @@ public class UserConverter {
         user.setPassword(userDTO.getPassword());
         user.setPhoneNumber(userDTO.getPhoneNumber());
         user.setFace(userDTO.getFace());
+        user.setJoinGroupSet(userDTO.getJoinGroupSet());
+        user.setCreateGroupSet(userDTO.getCreateGroupSet());
+        user.setYesTaskSet(userDTO.getYesTaskSet());
+        user.setNoTaskSet(userDTO.getNoTaskSet());
         return user;
     }
 }
