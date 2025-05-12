3d85399c5ba9b732cb80c40f4c25f7dc5cea4ce4
Muninn <2545908841@qq.com>
2025-05-12 23:02:40 +0800

    Merge remote-tracking branch 'origin/Dev-Muninn' into Dev-Muninn

--- src/main/java/com/WhiteDeer/exception/GroupNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.WhiteDeer.exception;
-
-public class GroupNotFoundException extends RuntimeException {
-    public GroupNotFoundException(String groupId) {
-        super("找不到ID为 " + groupId + " 的群组");
-    }
-}
--- src/main/java/com/WhiteDeer/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.WhiteDeer.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String userId) {
-        super("找不到ID为 " + userId + " 的用户");
-    }
-}
